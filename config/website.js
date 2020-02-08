6b77aa7181bbd77a5f698b568aaeb53846739f4a
Selvaganesh <selvaganeshrajam@gmail.com>
2020-02-08 08:56:18 +0530

    Config added

---
+++ config/website.js
@@ -0,0 +1,24 @@
+module.exports = {
+  pathPrefix: '/',
+  siteTitle: 'Gannys Dev Blog',
+  siteTitleAlt: 'Blog page of Selvaganesh',
+  siteTitleShort: 'Ganny Blog',
+  siteUrl: 'https://blog.gannydev.com',
+  siteLanguage: 'en',
+  siteDescription: 'This is where ganny post things!',
+  author: 'Selvaganesh Rajam',
+  googleAnalyticsID: '',
+
+  // Manifest and Progress color
+  themeColor: '#5348FF',
+  backgroundColor: '#2b2e3c',
+
+  // Social component
+  twitter: 'https://twitter.com/seganesa/',
+  github: 'https://github.com/ganny26/',
+  medium: 'https://medium.com/@selvaganesh93',
+  stackoverflow: 'https://stackoverflow.com/users/5845221/rselvaganesh',
+  instagram: 'https://www.instagram.com/selvaganesh93/',
+  youtube: 'https://www.youtube.com/user/ganeshrsg',
+  linkedin: 'https://in.linkedin.com/in/selvaganesh-rajam'
+}
